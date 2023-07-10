660ab6f555d8b99c5b92110a1107d4d59a8b63b0
abbysauce <am61428@email.vccs.edu>
2023-07-10 10:34:49 -0400

    hhh

--- .github/workflows/mathquiz.py
+++ .github/workflows/mathquiz.py
@@ -1,5 +1,13 @@
-from random 
-def generate_question():
-  num1= random.randint(1,10)
- num2= random.randint(1,10)
-question= "What is " + str(num1) + " times " + str(num2)
+from random import randint
+num1=randint(1,10)
+num2=randint(1,10)
+right=0
+print ("This quiz is timed")
+import time
+start_time= time.time()
+question= "What is " + str(num1) + " times " + str(num2) + "?"
+print (question)
+answer= int(input("Enter a number: "))
+if answer== (num1 * num2):
+    print("That is correct!")
+   right=right+1
