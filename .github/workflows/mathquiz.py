4f94452db27d70e66ecb77357b4b230980db469c
abbysauce <am61428@email.vccs.edu>
2023-07-10 10:50:58 -0400

    vrf

--- .github/workflows/mathquiz.py
+++ .github/workflows/mathquiz.py
@@ -13,5 +13,11 @@ if answer== (num1 * num2):
   right=right+1
 if answer!= (num1 * num2):
   print("Sorry that isnt correct")
-
-
+end_time= time.time()
+total_time=end_time-start_time
+print("Your total time was " + str(total_time))
+print(" I asked you 10 question. You got " + str(right) + " of them right.")
+if right<6:
+  print("You got less than six question right, you did not pass the quiz")
+if right>6:
+  print("Well done, you have passed the quiz")
