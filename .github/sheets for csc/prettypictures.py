8322df682b5b0b3f71bc2feb9bd6d6f408fa0b74
abbysauce <am61428@email.vccs.edu>
2023-07-12 11:41:16 -0400

    mainface

--- .github/sheets for csc/prettypictures.py
+++ .github/sheets for csc/prettypictures.py
@@ -1,8 +1,10 @@
 from gasp import *
 begin_graphics()
 Circle((300, 200), 100)
-Circle ((260, 220), 15)
-Circle((340, 220), 15)
+Circle((260, 220), 15)
+Circle((340,220),15)
+Line((280, 170), (320,170))
+Line((280,170),(300,220))
 Arc((300, 200), 70, 225, 315)
 update_when('key_pressed')
 end_graphics
