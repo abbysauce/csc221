975bca998a3c0ef2a60ff7432e4308330641165d
abbysauce <am61428@email.vccs.edu>
2023-07-16 18:10:12 -0400

    change

--- .github/sheets for csc/prettypictures.py
+++ .github/sheets for csc/prettypictures.py
@@ -1,7 +1,7 @@
 from gasp import *
 begin_graphics()
-Circle((300, 200), 100)
-Circle((260, 220), 15)
+Circle((300,200),100)
+Circle((260,220),15)
 Circle((340,220),15)
 Line((280, 170), (320,170))
 Line((280,170),(300,220))
