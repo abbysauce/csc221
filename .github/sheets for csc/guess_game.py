e79495be51699868edcb5bc945ae137665d97a38
abbysauce <am61428@email.vccs.edu>
2023-07-17 09:30:45 -0400

    while command

--- .github/sheets for csc/guess_game.py
+++ .github/sheets for csc/guess_game.py
@@ -5,11 +5,13 @@ print("I have thought of a number from 1 to 100")
 answer=int(input("Enter a number: "))
 if answer== number:
     print("Well done you have guessed my number")
-if answer>number:
+while answer > number:
     print("Try a lower number")
     userguess= userguess + 1
-if answer<number:
+    answer=int(input("Enter a number "))
+while answer < number:
     print("Try a higher number")
     userguess= userguess + 1
+    answer=int(input("Enter a number "))
 print("That was " + str(userguess)+ " guesses")
 print(" Would you like another game ")
