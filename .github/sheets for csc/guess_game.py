835ec00612ab2297972784f4a00f5ec56648723b
abbysauce <am61428@email.vccs.edu>
2023-07-17 09:54:39 -0400

    vrew

--- .github/sheets for csc/guess_game.py
+++ .github/sheets for csc/guess_game.py
@@ -2,16 +2,20 @@ from random import randint
 number= randint(1,100)
 userguess=0
 print("I have thought of a number from 1 to 100")
-answer=int(input("Enter a number: "))
-if answer== number:
-    print("Well done you have guessed my number")
-while answer > number:
-    print("Try a lower number")
-    userguess= userguess + 1
-    answer=int(input("Enter a number "))
-while answer < number:
-    print("Try a higher number")
-    userguess= userguess + 1
-    answer=int(input("Enter a number "))
+while True:
+   number=randint(1,100)
+   answer=int(input("Enter a guess "))
+   if answer > number:
+      print("Try a lower number")
+      userguess= userguess + 1
+      answer= int(input("Make another guess: "))
+   if answer < number:
+      print("Try a higher number")
+      userguess= userguess + 1
+      answer= int(input("Make another guess: "))
+   if answer == number:
+      break
+print("Well done that was my number! It was " + str(number))
 print("That was " + str(userguess)+ " guesses")
 print(" Would you like another game ")
+print(number)
