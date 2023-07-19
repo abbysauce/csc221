ca531df4c7606f079a3bf0867127fd4e0d0e1e80
abbysauce <am61428@email.vccs.edu>
2023-07-18 22:21:37 -0400

    efw

--- .github/sheets for csc/zombiegame.py
+++ .github/sheets for csc/zombiegame.py
@@ -1,13 +1,20 @@
+
 from gasp import *
 from random import randint
+begin_graphics()
+c=Circle((320, 200), 5)
+move_to(c, (300, 220))
+player_x= randint(0,63)
+player_y= randint(0,47)
 def place_player():
     print("here I am!")
 def move_player():
     print("I'm moving..")
-    update_when ('key pressed')
-begin_graphics()
+    update_when('key_pressed')
+Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
 finished = False
-place_player(player_x, player_y)
+place_player=((player_x, player_y), 5)
 while not finished:
   move_player()
-end_graphics()
+update_when('key_pressed')
+end_graphics
