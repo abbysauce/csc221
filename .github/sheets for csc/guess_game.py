a106d08e7a79e4e9427ce75b9f0cffce2118a236
abbysauce <am61428@email.vccs.edu>
2023-07-18 22:21:29 -0400

    sc

--- .github/sheets for csc/guess_game.py
+++ .github/sheets for csc/guess_game.py
@@ -1,9 +1,10 @@
 from random import randint 
+from gasp.utils import read_number
 number= randint(1,100)
 userguess=0
 print("I have thought of a number from 1 to 100")
 number= randint(1,100)
-answer= int(input("Enter a guess: "))
+answer= int(input("Enter a NUMBER: "))
 while True:
    if answer > number:
       print("Try a lower number")
@@ -17,4 +18,5 @@ while True:
       break
 print("Well done that was my number! It was " + str(number))
 print("That was " + str(userguess)+ " guesses")
-print(" Would you like another game ")
+from gasp.utils import read_yesorno
+print (read_yesorno + ('Would you like another game? '))
