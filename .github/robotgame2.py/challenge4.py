4ec77d15ba989d88b95f6a50f0b26170fabe9041
abbysauce <am61428@email.vccs.edu>
2023-07-24 10:30:18 -0400

    vre

--- .github/robotgame2.py/challenge4.py
+++ .github/robotgame2.py/challenge4.py
@@ -7,4 +7,7 @@ key_text = Text("a", (320, 240), size=48)
 while True:
     key = update_when('key_pressed')
     remove_from_screen(key_text)
-    key_text =Text
+    key_text = Text(key, (320, 240), size=48)
+    if key == 'q':
+        break
+end_graphics()
