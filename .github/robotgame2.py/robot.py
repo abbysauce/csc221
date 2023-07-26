9395848564f6ec328e47938aaa32055b6953278c
abbysauce <am61428@email.vccs.edu>
2023-07-26 10:02:16 -0400

    moving player

--- .github/robotgame2.py/robot.py
+++ .github/robotgame2.py/robot.py
@@ -1,31 +1,36 @@
 from gasp import *
 from random import randint
 
-player_x= randint(0,63)
-player_y= randint(0,47)
 
 def place_player():
-    Circle((10 * player_x + 5, 10 * player_y +5), 5, filled=True)
-    print("Here I am!")
+    global player_x, player_y, player_shape
+
+    player_x = randint(0, 63)
+    player_y = randint(0, 47)
+    player_shape = Circle((10 * player_x + 5, 10 * player_y +5), 5, filled=True)
+
 
 def move_player():
     global player_x, player_y, player_shape
 
     key = update_when ('key_pressed')
 
-    if key == '6' and player_x < 63:
+    if key == 'd' and player_x < 63:
         player_x += 1
-    elif key == '3':
+    elif key == 'c':
         if player_x < 63:
             player_x += 1
         if player_y >0:
             player_y -= 1
-    print("I'm moving...")
+    move_to(player_shape, (10 * player_x + 5, 10 * player_y +5))
+    
 
 begin_graphics()
 finished = False
 
 place_player()
+
+move_player()
 
 while not finished:
     move_player()
